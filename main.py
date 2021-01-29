85685d197ef6f831189910a5d0587b1a9562855d
unknown <serjumano17@gmail.com>
2021-01-29 13:05:52 -0300

    .

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from controllers.usersController import User
 from decouple import config
 
 PORT = config('PORT')
+api.add_resource(Product, '/product', '/product')
+api.add_resource(User, '/users', '/users')
 
 if __name__ == "__main__":
-  api.add_resource(Product, '/product', '/product')
-  api.add_resource(User, '/users', '/users')
-
   app.run(port=int(PORT), debug=True)
