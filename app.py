dad076201696b3c77a3d63ba907b72988a28a514
unknown <serjumano17@gmail.com>
2021-01-29 13:01:33 -0300

    .

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={'/product': {"origins": "*"}, '/users': {"origins": "*"}})
+CORS(app, resources={'/product': {"origins": "*"}, '/users': {"origins": "*"}, '/test': {"origins": "*"}})
+
+@app.route('/test')
+def test():
+  return {
+    'ok': 'ok'
+  }, 200
+
 api = Api(app)
