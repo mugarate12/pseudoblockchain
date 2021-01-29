509c6016430491698c11719d6c6a913de774f062
unknown <serjumano17@gmail.com>
2021-01-29 10:19:53 -0300

    .

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
-import random
 
 app = Flask(__name__)
 CORS(app, resources={'/product': {"origins": "*"}, '/users': {"origins": "*"}})
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from app import api, app
 from controllers.productController import Product
 from controllers.usersController import User
+from decouple import config
+
+PORT = config('PORT')
 
 if __name__ == "__main__":
   api.add_resource(Product, '/product', '/product')
   api.add_resource(User, '/users', '/users')
 
-  app.run(port=3333, debug=True)
+  app.run(port=int(PORT), debug=True)
